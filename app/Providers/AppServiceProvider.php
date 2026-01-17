0997480e03dca475d8329701d741b306ddd8da16
M0R4Xxx <jeremialintang4@gmail.com>
2026-01-17 16:01:09 +0700

    Siap deploy ke Vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// Tambahkan baris ini jika ingin memaksa HTTPS di Vercel
+use Illuminate\Support\Facades\URL; 
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // PENTING: Agar gambar & CSS terbaca di Vercel
+        $this->app->bind('path.public', function() {
+            return base_path('public');
+        });
+
+        // OPSIONAL: Jika tampilan website berantakan di Vercel, aktifkan ini:
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
