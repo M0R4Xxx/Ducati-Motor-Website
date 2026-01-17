20bf4830d2abd18fea89b3391dcd46ecabbe2623
M0R4Xxx <jeremialintang4@gmail.com>
2026-01-17 16:27:55 +0700

    Fix storage path for vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,9 @@
 <?php
+
+// Pastikan folder storage dan bootstrap/cache ada di memory
+// agar tidak error saat Laravel mencoba menulis cache
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+
 require __DIR__ . '/../public/index.php';
